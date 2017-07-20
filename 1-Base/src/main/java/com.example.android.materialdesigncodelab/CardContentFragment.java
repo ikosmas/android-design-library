f1519df10559117c0d94554d382c34356a9854b7
ikosmas <kosmas_giannis@hotmail.com>
2017-07-21 00:20:49 +0300

    Layout and Animation

---
+++ 1-Base/src/main/java/com.example.android.materialdesigncodelab/CardContentFragment.java
@@ -0,0 +1,10 @@
+package com.example.android.materialdesigncodelab;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by giannis on 7/20/2017.
+ */
+
+public class CardContentFragment extends Fragment{
+}
